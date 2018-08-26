4182918de4e08737d59df78e27350d0e60cfd0db
baminami <yuki7827@gmail.com>
2018-08-26 15:36:29 +0900

    【一覧画面】Todoのタイトルが空文字の時にエラーを出す #8対応 エラーを出すときは画面遷移をしないようにした。

--- Todoapl/AddViewController.swift
+++ Todoapl/AddViewController.swift
@@ -9,14 +9,25 @@ class AddViewController: UIViewController {
     //テキストフィールドの設定
     @IBOutlet weak var TodoTextField: UITextField!
     
+    //エラーメッセージを表示するラベルの設定
+    @IBOutlet weak var errorLabel: UILabel!
+    
     //追加ボタンの設定
     @IBAction func TodoAddButton(_ sender: Any) {
+        //空文字チェック
+        if let emptyCheckStr: String = TodoTextField.text,
+            emptyCheckStr.isEmpty {
+            errorLabel.text = "タイトルを記入してください"
+            return
+        }
         //変数に入力内容を入れる
         TodoKobetsunonakami.append(TodoTextField.text!)
         //追加ボタンを押したらフィールドを空にする
         TodoTextField.text = ""
         //変数の中身をUDに追加
         UserDefaults.standard.set( TodoKobetsunonakami, forKey: "TodoList" )
+        //追加画面に遷移
+        self.performSegue(withIdentifier: "addToMain", sender: nil)
     }
     
     //最初からあるコード
